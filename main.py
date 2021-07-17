53e7f05c6ae62c07d60f201dc42e22e028a06b74
SendiAp <86170325+SendiAp@users.noreply.github.com>
2021-07-18 02:01:39 +0700

    Update main.py

--- main.py
+++ main.py
@@ -57,7 +57,7 @@ def c_start(update: Update, ctx: CallbackContext) -> None:
 	"""General info about the bot and command help."""
 	
 	text = (
-		"Kirim Perintah Digrub 🌹",
+		"Ini adalah bot TrutOrDare khusus untuk grup, kirim command di grup.",
 	)
 	ctx.bot.send_message(chat_id = update.effective_chat.id, text = '\n'.join(text))
 
